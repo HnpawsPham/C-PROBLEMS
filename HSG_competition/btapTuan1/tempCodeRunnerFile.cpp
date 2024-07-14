7b968f7c5230dda29e458fa83110a36bfcca11b8
HnpawsPham <hanpham123th@gmail.com>
2024-07-14 21:26:56 +0700

    v1

---
+++ HSG_competition/btapTuan1/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+  // // be-lon
+    //  for (int i=1;i<=9;i++){
+    //     for (int j=i+1;j<=10;j++){
+    //         if (a[i]>a[j]){
+    //             temp=a[i];
+    //             a[i]=a[j];
+    //             a[j]=temp;
+    //         }
+    //     }
+    // }
+    //  cout<<"Mang sap xep tu bé-lớn: \n";
+    // for (int i=1;i<=10;i++){
+    //     cout<<a[i]<<" ";
+    // }
