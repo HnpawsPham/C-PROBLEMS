83d014c7e7351881c24e9449c69cf6040f1b1c9b
HnpawsPham <hanpham123th@gmail.com>
2024-08-26 23:50:44 +0700

    v25

--- CSES/FINDING-PATTERNS.cpp
+++ CSES/FINDING-PATTERNS.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string str, s;
+string str;
+int t;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+bool kmp(string s){
+    int n = str.size();
+    int m = s.size();
 
-    cin>>str>>s;
-    int n = str.length();
-    int m = s.length();
-    int lps[m + 1] = {0};
+    int lps[m];
+    lps[0] = 0;
 
     int i = 1, j = 0;
     while(i < m){
         if(s[i] == s[j]){
             j++;
-            lps[i] = j;
+            lps[i]= j;
             i++;
         }
         else{
@@ -32,16 +30,14 @@ int main(){
     }
 
     i = 0, j = 0;
-    int cnt = 0;
-
     while(i < n){
         if(str[i] == s[j]){
             i++;
             j++;
         }
+
         if(j == m){
-            cnt++;
-            j = lps[j - 1];
+            return true;
         }
         else if(i < n && str[i] != s[j]){
             if(j == 0){
@@ -53,6 +49,21 @@ int main(){
         }
     }
 
-    cout<<cnt<<endl;
+    return false;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin>>str>>t;
+    while(t--){
+        string s;
+        cin>>s;
+
+        cout<<(kmp(s) ? "YES" : "NO")<<endl;
+    }
+
     return 0;
 }
