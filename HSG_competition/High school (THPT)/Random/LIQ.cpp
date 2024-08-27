50cccf5b25b45af57a3a0cba2c13d89448f4938c
HnpawsPham <hanpham123th@gmail.com>
2024-08-27 23:53:18 +0700

    v26

--- HSG_competition/High school (THPT)/Random/LIQ.cpp
+++ HSG_competition/High school (THPT)/Random/LIQ.cpp
@@ -13,20 +13,24 @@ int main()
     cin >> n;
     int a[n + 1], cnt[n + 1] = {1}, maxlen = 0;
 
-    for(int i = 0;i<n;i++){
-        cin>>a[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
 
-    for(int i = 0;i<n;i++){
-        for(int j = 0; j < i;j++){
-            if(a[i] > a[j]){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (a[i] > a[j])
+            {
                 cnt[i] = max(cnt[i], cnt[j] + 1);
             }
         }
         maxlen = max(maxlen, cnt[i]);
     }
 
-    cout<<maxlen<<endl;
+    cout << maxlen << endl;
 
     return 0;
 }
