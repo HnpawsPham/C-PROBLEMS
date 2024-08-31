743c3f639c7f2d5d87f095e0259b3a23354e89cc
HnpawsPham <hanpham123th@gmail.com>
2024-08-31 23:51:31 +0700

    v29

--- random_practice/IVA_AND_PAV.cpp
+++ random_practice/IVA_AND_PAV.cpp
@@ -1,64 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define pii pair<int, int>
 
 int t;
 
-int find(int p[], int st, int n, ll k)
-{
-    int l = st - 1, r = n - 1;
-    int res = -1;
-
-    while (l <= r)
-    {
-        int mid = (l + r) / 2;
-
-        if (p[mid] >= k)
-        {
-            res = max(res, mid + 1);
-            l = mid + 1;
-        }
-        else
-        {
-            r = mid - 1;
-        }
+void build(int node, int st, int en, ll a[], ll tree[]){
+    if(st == en){
+        tree[node] = a[st];
+        return;
     }
 
-    return res;
+    int mid = (st + en) / 2;
+    build(2 * node, st, mid, a, tree);
+    build(2 * node + 1, mid + 1, en, a, tree);
+
+    tree[node] = tree[2 * node] & tree[2 * node + 1];
+    return;
+}
+
+ll sum(int node, int st, int en, int l, int r, ll a[], ll tree[]){
+    if(l > en || r < st){
+        return -1;
+    }
+    if(st >= l && en <= r){
+        return tree[node];
+    }
+
+    int mid = (st + en) / 2;
+    ll left = sum(2 * node, st, mid, l, r, a, tree);
+    ll right = sum(2 * node + 1, mid + 1, en, l, r, a, tree);
+
+    return left & right;
+}
+
+ll find(int node, int st, int en, int l , int k, ll curr, ll a[], ll tree[]){
+    if(st > en || en < l){
+        return -1;
+    }
+    if(st == en){
+        return ((curr & tree[node]) >= k ? st : -1);
+    }
+
+    int mid = (st + en) / 2;
+    ll left = sum(2 * node, st, mid, l, mid, a, tree);
+
+    if((curr & left) >= k){
+        int right = find(2 * node + 1, mid + 1, en, l, k, curr & left, a, tree);
+        return(right != -1 ? right : mid);
+    }
+
+    return find(2 * node, st, mid, l, k, curr, a, tree);
 }
 
 void solve()
 {
     int n, q;
     cin >> n;
-    ll a[n];
+    ll a[n + 1];
+    ll tree[4 * n + 1];
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
     }
+    build(1, 1, n, a, tree); 
 
-    cin >> q;
-    while (q--)
-    {
+    cin>>q;
+    while(q--){
         int l;
         ll k;
-        cin >> l >> k;
-
-        ll p[n];
-        for (int i = l - 1; i < n; i++){
-            p[i] = a[i];
-
-            if (i > 0){
-                p[i] &= p[i - 1];
-            }
-        }
-
-        int r = find(p, l, n, k);
-        cout << r << " ";
+        cin>>l>>k;
+        cout<<find(1, 1, n, l, k, a[l], a, tree)<<" ";
     }
-    
+
     cout << endl;
     return;
 }
