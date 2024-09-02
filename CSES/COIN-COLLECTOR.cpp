d73a0706b566c05b48497f53843a44f6b4ac8c30
HnpawsPham <hanpham123th@gmail.com>
2024-09-02 23:52:31 +0700

    v31

--- CSES/COIN-COLLECTOR.cpp
+++ CSES/COIN-COLLECTOR.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+#define el "\n";
 
 int n, m;
 const int maxn = 100001;
-vector<vector<int>> a(maxn);
-ll p[maxn];
-int idx[maxn], low[maxn];
+vector<vector<int>> a(maxn), sccs(maxn);
+ll p[maxn], sccSum[maxn], dp[maxn];
+int idx[maxn], low[maxn], sccId[maxn];
 bool check[maxn] = {false};
-int id = 0;
-ll res;
 stack<int> q;
+int id = 0, sccCnt = 0;
 
 void dfs(int u)
 {
@@ -25,7 +25,7 @@ void dfs(int u)
             dfs(v);
             low[u] = min(low[u], low[v]);
         }
-        else if (check[u])
+        else if (check[v])
         {
             low[u] = min(low[u], idx[v]);
         }
@@ -33,6 +33,8 @@ void dfs(int u)
 
     if (idx[u] == low[u])
     {
+        sccCnt++;
+
         ll sum = 0;
         while (true)
         {
@@ -40,16 +42,27 @@ void dfs(int u)
             q.pop();
 
             sum += p[v];
-
             check[v] = false;
-            low[v] = idx[u];
+            sccId[v] = sccCnt;
 
-            if (v == u)
-                break;
+            if (v == u) break;
         }
-        res = max(res, sum);
+        sccSum[sccCnt] = sum;
     }
     return;
+}
+
+ll getMax(int u)
+{
+    if (dp[u] != -1) return dp[u];
+
+    dp[u] = sccSum[u];
+    for (int v : sccs[u])
+    {
+        dp[u] = max(dp[u], getMax(v) + sccSum[u]);
+    }
+
+    return dp[u];
 }
 
 int main()
@@ -59,10 +72,8 @@ int main()
     cout.tie(0);
 
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> p[i];
-    }
+    for (int i = 1; i <= n; i++) cin >> p[i];
+    
     for (int i = 0; i < m; i++)
     {
         int u, v;
@@ -71,7 +82,6 @@ int main()
     }
 
     memset(idx, -1, sizeof(idx));
-
     for (int i = 1; i <= n; i++)
     {
         if (idx[i] == -1)
@@ -80,7 +90,22 @@ int main()
         }
     }
 
-    cout << res << "\n";
+    for (int u = 1; u <= n; u++)
+    {
+        for (int v : a[u])
+        {
+            if (sccId[v] == sccId[u]) continue;
+            sccs[sccId[u]].push_back(sccId[v]);
+        }
+    }
+    memset(dp, -1, sizeof(dp));
+
+    ll res = 0;
+    for (int i = 1; i <= sccCnt; i++)
+    {
+        res = max(res, getMax(i));
+    }
+    cout << res << el;
 
     return 0;
 }
